dbb0df551f264393877d7367d13d4fa0efba2855
MadMole <kippie1@msn.com>
2015-09-10 01:24:32 +0200

    Initial commit for new git repo

---
+++ Assets/CoinRotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinRotator : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (10, 45, 0) * Time.deltaTime);
+	}
+}
